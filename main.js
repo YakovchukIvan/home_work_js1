956640f9385f23fd7b67a9681cddcee4685058a9
Ivan Yakovchuk <litvinovjobs@gmail.com>
2021-12-03 22:18:05 +0200

    home_work_js

---
+++ main.js
@@ -0,0 +1,45 @@
+// Task1
+
+// login = prompt("Enter your username:");
+// alert ("Hello " + login)
+
+// Task2
+
+// number1 = +prompt("Enter your date of birth:");
+// conts = number2 = (2021);
+// const ("Your age " + (number2 - number1));
+
+// Task3
+
+// question = +prompt ("Set the size of the square:");
+// number1 = (4);
+// alert ("The size of the perimeter of the square " + (question * number1));
+
+// Task4
+
+// question = +prompt ("Enter the radius of the circle:");
+// number1 = (3.14);
+// alert ("The area of the circle is " + ((question * question) * number1 ) + " cm2");
+
+// Task5
+
+// question1 = +prompt ("Відстань між двома містами (в км):");
+// question2 = +prompt ("За скільки годин хочете дістатися?:");
+// number1 = (question1);
+// number2 = (question2);
+// alert ("Швидкість,яка потрібна щоб дістатися вчасно " + (number1 / number2))
+
+// Task6 
+
+// question1 = +prompt ("Введіть сумму в $:")
+// number1 = (question1);
+// conts =  number2 = (0.88);
+// alert ("Євро " + (number1 * number2));
+
+// Task7
+
+question1 = +prompt ("Вкажіть обсяг флешкі в Гб:");
+number1 = (question1);
+number2 = (1000);
+number3 = (820);
+alert ((number1 * number2) / number3)
